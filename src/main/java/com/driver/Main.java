6105fb129bc7d7baca86070b655fefcac05aab33
Shriniwas Tak <shriniwastak@gmail.com>
2023-08-12 23:50:25 +0530

    Encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args){
+        RWOnly c1=new RWOnly();
+        c1.setname("Acciojob");
+        System.out.print(c1.getname());
+
+
+    }
   
 }
